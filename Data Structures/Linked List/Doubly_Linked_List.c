bed3c8e611db5f0a5f6f65c3948a2c6cd580c67e
Suchismita Bose <suchismitabose29@gmail.com>
2023-09-22 00:45:30 +0530

    Add files via upload

--- Data Structures/Linked List/Doubly_Linked_List.c
+++ Data Structures/Linked List/Doubly_Linked_List.c
@@ -2,46 +2,49 @@
 #include<stdlib.h>
 typedef struct node
 {
-	int data; //actual data
-	struct node *link; //address of next node;
+	int data;
+	struct node *prev; // point the address of previous node
+	struct node *next; //point the address of next node
 }node;
 
-node *start, *temp, *ptr;
+node *start ,*temp,*ptr;
 void insertFirst()
 {
 	temp=(node*)malloc(sizeof(node));
 	printf("Enter the data : ");
 	scanf("%d",&temp->data);
-	temp->link=NULL;
+	temp->prev=NULL;
+	temp->next=NULL;
 	
 	if(start==NULL)
 	 start=temp;
 	else
 	{
-		temp->link=start;
+		temp->next=start;
+		start->prev=temp;
 		start=temp;
 	}
-	
 }
 void insertLast()
 {
 	temp=(node*)malloc(sizeof(node));
 	printf("Enter the data : ");
 	scanf("%d",&temp->data);
-	temp->link=NULL;
+	temp->prev=NULL;
+	temp->next=NULL;
 	
 	if(start==NULL)
 	 start=temp;
 	else
 	{
 		ptr=start;
-		while(ptr->link!=NULL) // until last node
-		 ptr=ptr->link; //move to the next node
-		
-		ptr->link=temp;
+		while(ptr->next!=NULL)
+		 ptr=ptr->next;
+		 
+		ptr->next=temp;
+		temp->prev=ptr;
 	}
 }
-
 void deleteFirst()
 {
 	if(start==NULL)
@@ -49,56 +52,82 @@ void deleteFirst()
 	else
 	{
 		printf("\n Delete Element %d",start->data);
-		start=start->link;
+		start=start->next;
 	}
 }
 void deleteLast()
 {
 	if(start==NULL)
 	 printf("\n U N D E R F L O W");
-	else if(start->link==NULL)
-	{	printf("\n Delete Element %d",start->data);
+	else if(start->next==NULL)
+	{
+		printf("\n Deleted Element %d",start->data);
 		start=NULL;
 	}
 	else
 	{
 		ptr=start;
-		while(ptr->link->link!=NULL)
-		 ptr=ptr->link;
+		while(ptr->next!=NULL)
+		 ptr=ptr->next;
+		 
+		ptr=ptr->prev;
 		
-		printf("\n Delete Element %d",ptr->link->data);
-		ptr->link=NULL;
+		ptr->next->prev=NULL;
+		ptr->next=NULL;
 	}
 }
 void display()
 {
 	if(start==NULL)
-	 printf("\n List is Empty ");
-	 
+	 printf("\n List is Empty");
 	else
 	{
 		ptr=start;
-		printf("\n Elements are : ");
+		printf("\n Elements are ..\n");
 		while(ptr!=NULL)
 		{
 			printf("\n %d",ptr->data);
-			ptr=ptr->link;
+			ptr=ptr->next;
 		}
 	}
 }
-int main()
+void search()
+{
+	int num,p=0;
+	if(start==NULL)
+	 printf("\n Search Not Possible ");
+	else
+	{
+		printf("\n Enter the data : ");
+		scanf("%d",&num);
+		ptr=start;
+		while(ptr!=NULL)
+		{
+			p++;
+			if(ptr->data==num)
+			 break;
+			ptr=ptr->next;
+		}
+		if(ptr==NULL)
+		 printf("\n Number not Present ");
+		else
+		 printf("\n Number present at position %d",p);
+	}
+}
+void main()
 {
 	int ch;
 	start=NULL;
 	while(1)
 	{
-		printf("\n 1 for Insert First ");
+		printf("\n\n 1 for Insert First ");
 		printf("\n 2 for Insert Last ");
 		printf("\n 3 for Delete First ");
 		printf("\n 4 for Delete Last ");
 		printf("\n 5 for Display ");
-		printf("\n 6 for Exit ");
-		printf("\n\n Enter choice ");
+		printf("\n 6 for Search ");
+		printf("\n 7 for Exit ");
+		printf("\n Enter choice ");
 		scanf("%d",&ch);
 		
 		switch(ch)
@@ -124,10 +153,14 @@ int main()
 			break;
 			
 			case 6:
+			search();
+			break;
+			
+			case 7:
 			exit(1);
 			
 			default:
-			printf("\n Oops! Wrong choice ");
+			printf("\n Wrong Choice ");
 		}
 	}
 }
