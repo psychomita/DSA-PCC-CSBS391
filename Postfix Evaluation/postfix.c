f1073b734a62d2db6edd6c3e045c3995e82826c1
Suchismita Bose <suchismitabose29@gmail.com>
2023-11-05 22:02:26 +0530

    Update postfix.c

--- Postfix Evaluation/postfix.c
+++ Postfix Evaluation/postfix.c
@@ -1,62 +1,65 @@
 #include<stdio.h>
-int stack[50];
-int top = -1;
-void push(int x)
+#include<string.h>
+#include<math.h>
+#define MAX 100
+void push(int stk[], int *top, int num)
 {
-    stack[++top] = x;
+	*top=*top+1;
+	stk[*top]=num;
 }
-
-int pop()
+int pop(int stk[], int *top)
 {
-    return stack[top--];
+	int n=stk[*top];
+	*top=*top-1;
+	return n;
 }
-
 int main()
 {
-    char exp[20];
-    char *p;
-    int n1,n2,n3,num;
-    printf("Enter the expression: ");
-    scanf("%[^\n]%*c",exp);
-    p = exp;
-    while(*p != '\0')
-    {
-        if(isdigit(*p))
-       {
-            num = *p - 48;
-            push(num);
-        }
-        else
-        {
-            n1 = pop();
-            n2 = pop();
-            switch(*p)
-            {
-            case '+':
-            {
-                n3 = n1 + n2;
-                break;
-            }
-            case '-':
-            {
-                n3 = n2 - n1;
-                break;
-            }
-            case '*':
-            {
-                n3 = n1 * n2;
-                break;
-            }
-            case '/':
-            {
-                    n3 = n2 / n1;
-                break;
-            }
-            }
-            push(n3);
-        }
-        p++;
-    }
-    printf("\nThe result of expression %s = %d\n",exp,pop());
-    return 0;
+	char post[MAX],w[MAX];
+	int stack[MAX];
+	int i,j=0,k,num=0,top=-1,A,B,R;
+	printf("Enter the expression ");
+	gets(post);
+	strcat(post," ");
+	for(i=0;i<strlen(post);i++)
+	{
+		if(post[i]!=' ')
+		 w[j++]=post[i];
+		else
+		{
+			w[j]='\0';
+			if(strlen(w)==1 && (w[0]=='+' || w[0]=='-' || w[0]=='*' || w[0]=='/' || w[0]=='^' || w[0]=='%'))
+			{
+				B=pop(stack,&top);
+				A=pop(stack,&top);
+				printf("\n");
+				if(w[0]=='+')
+				 R=A+B;
+				else if(w[0]=='-')
+				 R=A-B;
+				else if(w[0]=='*')
+				 R=A*B;
+				else if(w[0]=='/')
+				 R=A/B;
+				else if(w[0]=='%')
+				 R=A%B;
+				else
+				 R=pow(A,B);
+				
+				push(stack,&top,R);
+			}
+			else
+			{
+				num=0;
+				for(k=0;k<strlen(w);k++)//15 0-9 ASCII 48-57
+				 num=num*10+(int)w[k]-48; //15
+				 
+				 push(stack,&top,num);
+				
+			}
+			j=0;
+		}
+		 
+	}
+	printf("\n Result %d",stack[top]);
 }
