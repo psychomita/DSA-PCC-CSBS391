224cb0fb25867939c7b0f5c3b904fa1701d65415
Suchismita Bose <suchismitabose29@gmail.com>
2023-11-05 21:57:13 +0530

    Update infix_postfix.c

--- Infix to Postfix Conversion/infix_postfix.c
+++ Infix to Postfix Conversion/infix_postfix.c
@@ -1,56 +1,67 @@
+//infix to postfix
 #include<stdio.h>
-#include<ctype.h>
-char stack[100];
-int top = -1;
-void push(char x)
+#include<string.h>
+#define MAX 100
+int priority(char ch)
 {
-    stack[++top] = x;
+	if(ch=='^')
+	 return 4;
+	else if(ch=='*' || ch=='/' || ch=='%')
+	 return 3;
+	else if(ch=='+' || ch=='-')
+	 return 2;
+	else
+	 return 1;
 }
-char pop()
+void push(char stk[], int *top,char op)
 {
-    if(top == -1)
-        return -1;
-    else
-        return stack[top--];
+	*top=*top+1;
+	 stk[*top]=op;
 }
-int priority(char x)
+char pop(char stk[], int *top)
 {
-    if(x == '(')
-        return 0;
-    if(x == '+' || x == '-')
-        return 1;
-    if(x == '*' || x == '/')
-        return 2;
-    return 0;
+	char op=stk[*top];
+	*top=*top-1;
+	return op;
 }
 int main()
 {
-    char exp[100];
-    char *e, x;
-    printf("Enter the infix expression: ");
-    scanf("%[^\n]%*c",exp);
-    printf("\nThe corresponding postfix expression is:\n");
-    e = exp;
-    while(*e != '\0')
-    {
-        if(isalnum(*e))
-            printf("%c ",*e);
-        else if(*e == '(')
-            push(*e);
-        else if(*e == ')')
-        {
-            while((x = pop()) != '(')
-                printf("%c ", x);
-        }
-        else
-        {
-            while(priority(stack[top]) >= priority(*e))
-                printf("%c ",pop());
-            push(*e);
-        }
-        e++;
-    }
-    while(top!=-1)
-        printf("%c ",pop());
-    return 0;
+	char infix[MAX],postfix[MAX],stack[MAX];
+	int top=-1;// initialize
+	int i,ind=0;
+	printf("Enter the infix expression : ");
+	scanf("%s",infix);
+	strupr(infix);
+	for(i=0;i<strlen(infix);i++)
+	{
+		if(infix[i]=='(')
+		 push(stack,&top,infix[i]);
+		
+		else if(infix[i]>='A' && infix[i]<='Z')
+		 postfix[ind++]=infix[i];
+		 
+		else if(infix[i]==')')
+		{
+			while(stack[top]!='(')
+			 postfix[ind++]=pop(stack,&top);
+			
+			top--; //'Remove ( from stack top
+		}
+		else //operator
+		{
+			if(priority(infix[i])>priority(stack[top]))
+			 push(stack,&top,infix[i]);
+			else
+			{
+				while(priority(infix[i])<=priority(stack[top]))
+				 postfix[ind++]=pop(stack,&top);
+				
+				push(stack,&top,infix[i]);//operator place stack top
+			}
+		}
+		stack[++top]='\0';
+		postfix[ind]='\0';
+		printf("\n %c\t%s\t%s",infix[i],stack,postfix);
+		top--;
+	}//end of loop
 }
