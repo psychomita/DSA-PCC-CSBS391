1e56b73341272ea5dc04b848732b20d73ed77fab
Suchismita Bose <suchismitabose29@gmail.com>
2023-12-10 00:46:56 +0530

    Update quick.c

--- Quick Sort/quick.c
+++ Quick Sort/quick.c
@@ -1,48 +1,51 @@
 #include<stdio.h>
-#include<stdlib.h>
-void swap(int *a, int *b)
+#define MAX 100
+void swap(int *x, int *y)
 {
-	int c=*a;
-	*a=*b;
-	*b=c;
+	int t=*x;
+	*x=*y;
+	*y=t;
 }
-int partition(int arr[], int lb, int ub) //partitioning using pivot element
-{ 
-	int pivot=arr[ub]; //choosing pivot element
-	int i=lb-1,j;
-	for(j=lb;j<=ub-1;j++)
+void quicksort(int a[], int f, int l)
+{
+	int pivot,i,j;
+	if(f<l)//multiple element
 	{
-		if(arr[j]<pivot)
+		pivot=a[f];
+		i=f,j=l;
+		while(i<=j)
 		{
-			i++;
-			swap(&arr[i],&arr[j]);
+			while(i<=j && pivot>=a[i])
+			 i++;
+			while(pivot<a[j])
+			 j--;
+			 
+			if(i<j)
+			 swap(&a[i],&a[j]);
 		}
-	}
-	swap(&arr[i+1],&arr[ub]);
-	return(i+1);
-}
-void sort(int arr[], int lb, int ub) //lb is the lower bound //ub is the upper bound
-{				      
-	if(lb<ub)
-	{
-		int part=partition(arr,lb,ub); //part is the partitioning index
-		sort(arr,lb,part-1);
-		sort(arr,part+1,ub);
+		swap(&a[f],&a[j]);
+		quicksort(a,f,j-1);
+		quicksort(a,j+1,l);
 	}
 }
 int main()
 {
-	int n,i;
-	printf("Enter the number of elements : ");
-	scanf("%d", &n);
-	int *arr=malloc(n*sizeof(int));
-	printf("Enter the elements : \n");
+	int a[MAX];
+	int i,n;
+	printf("Enter the size ");
+	scanf("%d",&n);
 	for(i=0;i<n;i++)
-		scanf("%d", &arr[i]);
-	sort(arr,0,n-1);
-	printf("The sorted elements are : \n");
+	{
+		printf("Enter the number ");
+		scanf("%d",&a[i]);
+	}
+	printf("\n Before Sorting ");
 	for(i=0;i<n;i++)
-		printf("%d\t",arr[i]);
-	printf("\n");
-	return 0;
+	 printf(" %d",a[i]);
+	
+	quicksort(a,0,n-1);
+	printf("\n After Sorting ");
+	for(i=0;i<n;i++)
+	 printf(" %d",a[i]);
+	
 }
